f346a818ceb0b22a365c925dd15fadcfefc1b7e1
Martin909 <>
2021-09-19 16:39:36 +0300

    add an alert to all the buttons

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
  function button1(){
-
+   alert("added to cart");
  }
 
  function button2(){
-
+   alert("added to cart");
  }
 
  function button3(){
-
+   alert("added to cart");
  }
 
  function button4(){
-
+   alert("added to cart");
  }
 
  function button5(){
-
+   alert("added to cart");
  }
 
  function button6(){
-
+   alert ("added to cart");
  }

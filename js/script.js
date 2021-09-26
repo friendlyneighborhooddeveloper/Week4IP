a576e98cab9e8458f1f9921e95cd696791eb3dbc
Martin909 <>
2021-09-26 15:15:57 +0300

    modifies alert( ) method

--- js/script.js
+++ js/script.js
@@ -1,23 +1,38 @@
- function button1(){
-   alert("added to cart");
- }
 
- function button2(){
-   alert("added to cart");
- }
 
- function button3(){
-   alert("added to cart");
- }
 
- function button4(){
-   alert("added to cart");
- }
 
- function button5(){
-   alert("added to cart");
- }
 
- function button6(){
-   alert ("added to cart");
+
+
+
+
+
+
+
+
+
+
+  function button1(){
+    alert("added to cart");
+  }
+
+  function button2(){
+    alert("added to cart");
+  }
+
+  function button3(){
+    alert("added to cart");
+  }
+
+  function button4(){
+    alert("added to cart");
+  }
+
+  function button5(){
+    alert("added to cart");
+  }
+
+  function button6(){
+   alert ("YOUR ORDER IS ON ITS WAY") 
  }
